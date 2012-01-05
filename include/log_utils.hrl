5bd58e42e0a4204dccf3a074fdac8abe1a5ed7ce
Motiejus Jakštys <desired.mta@gmail.com>
2012-01-05 15:05:41 +0200

    Recover log_utils

--- include/log_utils.hrl
+++ include/log_utils.hrl
@@ -1,19 +1,15 @@
-%-ifndef(LOG_UTILS__HRL).
-%-define(LOG_UTILS__HRL, true).
-%
-%-define(source_pos, ?FILE ++ ":" ++ integer_to_list(?LINE)).
-%
-%-define(error(Opts),
-%    error_logger:error_report
-%        ([{pid, self()}|[{source, ?source_pos}|Opts]])).
-%-define(warning(Opts),
-%    error_logger:warning_report
-%        ([{pid, self()}|[{source, ?source_pos}|Opts]])).
-%-define(info(Opts),
-%    error_logger:info_report
-%        ([{pid, self()}|[{source, ?source_pos}|Opts]])).
+-define(source_pos, ?FILE ++ ":" ++ integer_to_list(?LINE)).
+
+-define(error(M, Opts),
+    error_logger:error_report
+        ([{pid, self()}|[{source, ?source_pos}|[{message, M}|Opts]]])).
+-define(warning(M, Opts),
+    error_logger:warning_report
+        ([{pid, self()}|[{source, ?source_pos}|[{message, M}|Opts]]])).
+-define(info(M, Opts),
+    error_logger:info_report
+        ([{pid, self()}|[{source, ?source_pos}|[{message, M}|Opts]]])).
+
 
 -define(mute_log(), error_logger:tty(false)).
 -define(unmute_log(), error_logger:tty(true)).
-
-%-endif.
