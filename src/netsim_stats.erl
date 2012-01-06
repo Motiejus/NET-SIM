2feb4c75a143c4af25332822df1454e57f4b55c4
Motiejus Jakštys <desired.mta@gmail.com>
2012-01-06 03:16:11 +0200

    Refactor callbacks and add functional test

--- src/netsim_stats.erl
+++ src/netsim_stats.erl
@@ -9,13 +9,13 @@
         handle_info/2, terminate/2]).
 
 %%% API
--export([start_link/0, add_del_node/1, add_route/2, del_route/2,
-        define_resource/2]).
+-export([start_link/0, modify_resource/1,
+        add_route/2, del_route/2, define_resource/3]).
 
 -record(state, {
         res = update_this :: netsim_types:resource(),
-        waiting_for = add_resource :: netsim_types:action(),
         upto :: pos_integer(),
+        callback :: function(),
         sofar = [{add, []}, {del, []}] %:: [{add,
 %                [{netsim_types:latency(), pos_integer()}]
 %            }, {del, 
@@ -27,35 +27,35 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% @doc API
-add_del_node(Event=#event{}) ->
-    gen_server:call(?MODULE, {add_del_node, Event}).
+modify_resource(Event=#event{}) ->
+    gen_server:call(?MODULE, {modify_resource, Event}).
 
 %% @doc API for adding and deleting route
 -spec add_route(When :: netsim_types:latency(), netsim_types:resource()) -> ok.
 add_route(When, Resource) ->
-    gen_server:call({add_del_route, add, When, Resource}).
+    gen_server:call(?MODULE, {add_del_route, add, When, Resource}).
 -spec del_route(When :: netsim_types:latency(), netsim_types:resource()) -> ok.
 del_route(When, Resource) ->
-    gen_server:call({add_del_route, del, When, Resource}).
+    gen_server:call(?MODULE, {add_del_route, del, When, Resource}).
 
--spec define_resource(netsim_types:resource(), integer()) -> ok.
-define_resource(Resource, Upto) ->
-    gen_server:call({define_resource, Resource, Upto}).
+-spec define_resource(netsim_types:resource(), integer(), function()) -> ok.
+define_resource(Resource, NumNodes, Fun) ->
+    gen_server:call(?MODULE, {define_resource, Resource, NumNodes, Fun}).
 
 
 %% @doc Define resource
-handle_call({define_resource, Res, Upto}, _, State=#state{}) ->
-    {reply, ok, State#state{res=Res, upto=Upto}};
+handle_call({define_resource, Res, Upto, Fun}, _, State=#state{}) ->
+    {reply, ok, State#state{res=Res, upto=Upto, callback=Fun}};
 
 %% @doc Add or delete node
-handle_call({add_del_node, _}, _, State=#state{res=update_this}) ->
+handle_call({modify_resource, _E}, _, State=#state{res=update_this}) ->
     {reply, tell_resource_before_collecting, State};
 
-handle_call({add_del_node, #event{time=T, action=A, resource=Res}}, _From,
-    State=#state{res=Res, waiting_for=A, sofar=Sofar}) ->
+handle_call({modify_resource, #event{time=T, action=A, resource=Res}},
+    _From, State=#state{res=Res, sofar=Sofar}) ->
     {reply, ok, State#state{sofar=set_value(A, [{T, 0}], Sofar)}};
 
-handle_call({add_del_node, #event{}}, _From, State=#state{}) ->
+handle_call({modify_resource, #event{}}, _From, State=#state{}) ->
     {reply, ok, State};
 
 %% @doc Add or delete route to Resource, update statistics
@@ -73,8 +73,8 @@ handle_call({add_del_route, A, T, Res}, _From,
     UpTo = State#state.upto,
     case LastEntered of
          UpTo ->
-            lager:info("All complete: ~p", [NewAcc]);
-        _ when LastEntered > State#state.upto ->
+             (State#state.callback)(NewAcc);
+         _ when LastEntered > State#state.upto ->
             lager:error("Too many nodes: ~p", [NewAcc]);
         _ -> ok
     end,
@@ -103,23 +103,52 @@ set_value(K, V, Proplist) ->
 
 stats_test_() ->
     {foreach,
-        fun start_link/0,
+        fun setup/0,
         fun cleanup/1,
         [
-            fun test_add_node_before/0,
+            fun test_add_resource_before/0,
             fun test_functional/0
         ]
     }.
 
-test_add_node_before() ->
+test_add_resource_before() ->
     ?assertEqual(
         tell_resource_before_collecting,
-        add_del_node(#event{resource=x})
+        modify_resource(#event{resource=x})
     ).
 
 test_functional() ->
-    ok.
+    Rcpt = self(),
+    define_resource({a,1}, 5, fun(Arr) -> Rcpt ! Arr end), % 5 nodes in the graph
+    modify_resource(#event{time=3, action=add, resource={a,1}}),
+    add_route(3, {a,1}),
+    add_route(5, {a,1}),
+    add_route(5, {a,1}),
+    add_route(6, {a,1}),
+    add_route(7, {a,1}),
+    ?assertEqual([{7,5},{6,4},{5,3},{3,1}],
+        receive A -> A end),
+    modify_resource(#event{time=20, action=del, resource={a,1}}),
+    del_route(21, {a,1}),
+    del_route(21, {b,2}), % garbage
+    del_route(22, {a,1}),
+    del_route(23, {a,1}),
+    del_route(30, {a,1}),
+    del_route(29, {c,2}), % garbage
+    del_route(30, {a,1}),
+    ?assertEqual([{30,5}, {23,3}, {22,2}, {21,1}, {20,0}],
+        receive A -> A end).
 
-cleanup(_) -> ok.
+
+setup() ->
+    ?mute_log(),
+    ok = application:start(lager),
+    ?unmute_log(),
+    start_link().
+
+cleanup(_) ->
+    ?mute_log(),
+    application:stop(lager),
+    ?unmute_log().
 
 -endif.
