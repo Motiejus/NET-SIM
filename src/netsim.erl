4b5b1381fbf26329e91838af5c3239c46a889635
Motiejus Jakštys <desired.mta@gmail.com>
2012-01-06 11:26:39 +0200

    Accept command-line arguments

--- src/netsim.erl
+++ src/netsim.erl
@@ -3,50 +3,27 @@
 
 -behavior(application).
 
--export([start_app/0]).
+-export([start_app/0, start_app/1]).
 -export([start/2, stop/1]).
 
 start_app() ->
-    ok = application:start(lager),
-    ok = application:start(netsim),
-
-    % Test data:
-    netsim_bootstrap:init(
+    start_app([
         filename:join(["priv", "nodelist.txt"]),
         filename:join(["priv", "channels.txt"]),
         filename:join(["priv", "simulation.txt"]),
         filename:join(["priv", "max_latency.txt"])
-    ).
+    ]).
+
+start_app(Args=[NodeList, Channels, Simulation, MaxLatency]) ->
+    ok = application:start(lager),
+    ok = application:start(netsim),
+
+    % Test data:
+    lager:info("Files: ~p", [Args]),
+    netsim_bootstrap:init(NodeList, Channels, Simulation, MaxLatency).
 
 start(_, _) ->
     {ok, _Pid} = netsim_sup:start_link().
 
 stop(_State) ->
     ok.
-
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
-
-integration_test_() ->
-    {foreach,
-        fun start_app/0,
-        fun cleanup/1,
-        [
-            {"Yadda test", fun yadda/0}
-        ]
-    }.
-
-yadda() ->
-    % @todo Replace with a fully deterministic thing
-    timer:sleep(5000),
-    %netsim_clock_serv:sync_state(finalize),
-    ok.
-
-cleanup(_) ->
-    ?mute_log(),
-    application:stop(sasl),
-    application:stop(netsim),
-    application:stop(lager),
-    ?unmute_log().
-
--endif.
