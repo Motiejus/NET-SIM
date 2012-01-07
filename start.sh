aee71572c06fc39cbd72a7f421fed0671aba3c68
Motiejus Jakštys <desired.mta@gmail.com>
2012-01-08 01:25:33 +0200

    Add safety checks for start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./rebar compile
-erl -config priv/silent_run.config -pa ebin/ -pa deps/*/ebin/ -boot start_sasl -s netsim start_app -s init stop -noshell -noinput
+./rebar compile || exit 1
+erl -config priv/silent_run.config -pa ebin/ -pa deps/*/ebin/ -boot start_sasl -s netsim start_app -s init stop -noshell -noinput || exit 1
 echo -n "Drawing plot ... "
-gnuplot -e ' call "time_percent.gp" "res/output.png" "res/output.txt"'
+gnuplot -e ' call "generator/time_percent.gp" "res/output.png" "res/output.txt"' || exit 1
 echo "done"
 echo "Plot written to res/output.png"
