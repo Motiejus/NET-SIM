926898429e763a66486eb404e991a4a55668599f
Martynas Pumputis <martynas.pumputis@gmail.com>
2012-01-07 22:40:38 +0200

    Update start.sh
    Close erl after generating logs

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./rebar compile
-erl -config silent_run.config -pa ebin/ -pa deps/*/ebin/ -boot start_sasl -s netsim start_app -noshell -noinput
+erl -config silent_run.config -pa ebin/ -pa deps/*/ebin/ -boot start_sasl -s netsim start_app -s init stop
 echo -n "Drawing plot ... "
 gnuplot -e ' call "time_percent.gp" "res/output.png" "res/output.txt"'
 echo "done"
