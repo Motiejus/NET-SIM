3512e8b8d6504aca106850d50f1daf96abb6b967
Motiejus Jakštys <desired.mta@gmail.com>
2012-01-08 03:30:07 +0200

    Define 10x10 and 100x100 graphs

--- generator/graph.py
+++ generator/graph.py
@@ -9,8 +9,8 @@ from graphlib import Graph
 PROJECT_DIR = path.abspath(path.dirname(path.dirname(__file__)))
 RES_DIR = path.join(PROJECT_DIR, 'res')
 
-def write_graph_data(graph):
-    DIR = path.join(RES_DIR, '%dx%d' % (graph.dimensions, graph.dimensions))
+def write_graph_data(graph, name):
+    DIR = path.join(RES_DIR, name)
     try:
         makedirs(DIR)
     except OSError: # directory probably exists, overwriting
@@ -24,7 +24,52 @@ def write_graph_data(graph):
     with open(path.join(DIR, 'settings.txt'), 'w') as f:
         f.write(graph.settings_file())
 
+class mkgraph:
+    def __init__(self, **kargs):
+        self.kargs = kargs
+    def __enter__(self):
+        return Graph(**self.kargs)
+    def __exit__(self, type, value, traceback):
+        pass
+
 if __name__ == "__main__":
-    # Create and dump simple 5x5 graph
-    graph_5x5 = Graph(dimensions=5, latency=(5, 5), bandwidth=(100, 100))
-    write_graph_data(graph_5x5)
+    # 10x10 nice order, all fixed
+    with mkgraph(dimensions=10, latency=(5, 5), bandwidth=(100, 100)) as g:
+        write_graph_data(g, "10x10_a_fix")
+
+    # 10x10 modified order, all fixed
+    with mkgraph(dimensions=10, latency=(5, 5), bandwidth=(100, 100)) as g:
+        g.nodes[0][0].link(g.nodes[2][5], latency=8, bandwidth=500)
+        g.nodes[5][0].link(g.nodes[5][9], latency=8, bandwidth=500)
+        write_graph_data(g, "10x10_b_fix")
+
+    # 10x10 nice order, some randomness
+    with mkgraph(dimensions=10, latency=(2, 5), bandwidth=(50, 500)) as g:
+        write_graph_data(g, "10x10_a_rand")
+
+    # 10x10 modified order, some randomness
+    with mkgraph(dimensions=10, latency=(2, 5), bandwidth=(50, 500)) as g:
+        g.nodes[0][0].link(g.nodes[2][5], latency=8, bandwidth=500)
+        g.nodes[5][0].link(g.nodes[5][9], latency=8, bandwidth=500)
+        write_graph_data(g, "10x10_m_rand")
+
+
+    # 100x100 nice order, all fixed
+    with mkgraph(dimensions=100, latency=(5, 5), bandwidth=(100, 100)) as g:
+        write_graph_data(g, "10x10_a_fix")
+
+    # 100x100 modified order, all fixed
+    with mkgraph(dimensions=100, latency=(5, 5), bandwidth=(100, 100)) as g:
+        g.nodes[0][0].link(g.nodes[20][30], latency=8, bandwidth=500)
+        g.nodes[10][0].link(g.nodes[40][0], latency=8, bandwidth=500)
+        write_graph_data(g, "100x100_b_fix")
+
+    # 100x100 nice order, some randomness
+    with mkgraph(dimensions=100, latency=(2, 5), bandwidth=(50, 500)) as g:
+        write_graph_data(g, "100x100_a_rand")
+
+    # 100x100 modified order, some randomness
+    with mkgraph(dimensions=100, latency=(2, 5), bandwidth=(50, 500)) as g:
+        g.nodes[0][0].link(g.nodes[20][30], latency=8, bandwidth=500)
+        g.nodes[10][0].link(g.nodes[40][0], latency=8, bandwidth=500)
+        write_graph_data(g, "100x100_m_rand")
